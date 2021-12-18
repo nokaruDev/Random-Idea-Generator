165c86eb7f83a40901f369b97635b271022669e4
MSI Leopard <MSI Leopard@DESKTOP-MLVUJBD>
2021-12-18 23:33:29 +0100

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,390 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class RIG
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txIdea = new System.Windows.Forms.TextBox();
+            this.txOpis = new System.Windows.Forms.Label();
+            this.txTitle = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txFiltr = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.ButtonGene1 = new System.Windows.Forms.Button();
+            this.ButtonGene2 = new System.Windows.Forms.Button();
+            this.ButtonGene4 = new System.Windows.Forms.Button();
+            this.ButtonGene3 = new System.Windows.Forms.Button();
+            this.ButtonGene8 = new System.Windows.Forms.Button();
+            this.ButtonGene7 = new System.Windows.Forms.Button();
+            this.ButtonGene6 = new System.Windows.Forms.Button();
+            this.ButtonGene5 = new System.Windows.Forms.Button();
+            this.ButtonGene10 = new System.Windows.Forms.Button();
+            this.ButtonGene9 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txIdea
+            // 
+            this.txIdea.Location = new System.Drawing.Point(107, 54);
+            this.txIdea.Name = "txIdea";
+            this.txIdea.Size = new System.Drawing.Size(219, 20);
+            this.txIdea.TabIndex = 0;
+            // 
+            // txOpis
+            // 
+            this.txOpis.AutoSize = true;
+            this.txOpis.Location = new System.Drawing.Point(3, 61);
+            this.txOpis.Name = "txOpis";
+            this.txOpis.Size = new System.Drawing.Size(84, 13);
+            this.txOpis.TabIndex = 1;
+            this.txOpis.Text = "Wybierz od 1-10";
+            // 
+            // txTitle
+            // 
+            this.txTitle.AutoSize = true;
+            this.txTitle.Location = new System.Drawing.Point(104, 9);
+            this.txTitle.Name = "txTitle";
+            this.txTitle.Size = new System.Drawing.Size(121, 13);
+            this.txTitle.TabIndex = 4;
+            this.txTitle.Text = "Random Idea Generator";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "5-Plane Crazy";
+            // 
+            // txFiltr
+            // 
+            this.txFiltr.AutoSize = true;
+            this.txFiltr.Location = new System.Drawing.Point(24, 28);
+            this.txFiltr.Name = "txFiltr";
+            this.txFiltr.Size = new System.Drawing.Size(31, 13);
+            this.txFiltr.TabIndex = 6;
+            this.txFiltr.Text = "Filtry:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 257);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(131, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "6-Adobe Photoshop & Gimp";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 312);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "8-ARK";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(20, 284);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "7-Terraria";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(20, 201);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(105, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "4-People Playground";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(20, 173);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(67, 13);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "3-The Forest";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(20, 146);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(60, 13);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "2-Minecraft";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(20, 118);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(72, 13);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "1-Garry\'s Mod";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(20, 366);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(58, 13);
+            this.label12.TabIndex = 15;
+            this.label12.Text = "10-Blender";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(20, 338);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(86, 13);
+            this.label13.TabIndex = 14;
+            this.label13.Text = "9-Montaż Filmów";
+            // 
+            // ButtonGene1
+            // 
+            this.ButtonGene1.Location = new System.Drawing.Point(150, 140);
+            this.ButtonGene1.Name = "ButtonGene1";
+            this.ButtonGene1.Size = new System.Drawing.Size(75, 23);
+            this.ButtonGene1.TabIndex = 16;
+            this.ButtonGene1.Text = "Generuj1!";
+            this.ButtonGene1.UseVisualStyleBackColor = true;
+            this.ButtonGene1.Click += new System.EventHandler(this.ButtonGene_Click);
+            // 
+            // ButtonGene2
+            // 
+            this.ButtonGene2.Location = new System.Drawing.Point(150, 169);
+            this.ButtonGene2.Name = "ButtonGene2";
+            this.ButtonGene2.Size = new System.Drawing.Size(75, 23);
+            this.ButtonGene2.TabIndex = 17;
+            this.ButtonGene2.Text = "Generuj2!";
+            this.ButtonGene2.UseVisualStyleBackColor = true;
+            this.ButtonGene2.Click += new System.EventHandler(this.ButtonGene2_Click);
+            // 
+            // ButtonGene4
+            // 
+            this.ButtonGene4.Location = new System.Drawing.Point(150, 226);
+            this.ButtonGene4.Name = "ButtonGene4";
+            this.ButtonGene4.Size = new System.Drawing.Size(75, 23);
+            this.ButtonGene4.TabIndex = 19;
+            this.ButtonGene4.Text = "Generuj4!";
+            this.ButtonGene4.UseVisualStyleBackColor = true;
+            this.ButtonGene4.Click += new System.EventHandler(this.ButtonGene4_Click);
+            // 
+            // ButtonGene3
+            // 
+            this.ButtonGene3.Location = new System.Drawing.Point(150, 197);
+            this.ButtonGene3.Name = "ButtonGene3";
+            this.ButtonGene3.Size = new System.Drawing.Size(75, 23);
+            this.ButtonGene3.TabIndex = 18;
+            this.ButtonGene3.Text = "Generuj3!";
+            this.ButtonGene3.UseVisualStyleBackColor = true;
+            this.ButtonGene3.Click += new System.EventHandler(this.ButtonGene3_Click);
+            // 
+            // ButtonGene8
+            // 
+            this.ButtonGene8.Location = new System.Drawing.Point(150, 339);
+            this.ButtonGene8.Name = "ButtonGene8";
+            this.ButtonGene8.Size = new System.Drawing.Size(75, 23);
+            this.ButtonGene8.TabIndex = 23;
+            this.ButtonGene8.Text = "Generuj8!";
+            this.ButtonGene8.UseVisualStyleBackColor = true;
+            this.ButtonGene8.Click += new System.EventHandler(this.ButtonGene8_Click);
+            // 
+            // ButtonGene7
+            // 
+            this.ButtonGene7.Location = new System.Drawing.Point(150, 310);
+            this.ButtonGene7.Name = "ButtonGene7";
+            this.ButtonGene7.Size = new System.Drawing.Size(75, 23);
+            this.ButtonGene7.TabIndex = 22;
+            this.ButtonGene7.Text = "Generuj7!";
+            this.ButtonGene7.UseVisualStyleBackColor = true;
+            this.ButtonGene7.Click += new System.EventHandler(this.ButtonGene7_Click);
+            // 
+            // ButtonGene6
+            // 
+            this.ButtonGene6.Location = new System.Drawing.Point(150, 282);
+            this.ButtonGene6.Name = "ButtonGene6";
+            this.ButtonGene6.Size = new System.Drawing.Size(75, 23);
+            this.ButtonGene6.TabIndex = 21;
+            this.ButtonGene6.Text = "Generuj6!";
+            this.ButtonGene6.UseVisualStyleBackColor = true;
+            this.ButtonGene6.Click += new System.EventHandler(this.ButtonGene6_Click);
+            // 
+            // ButtonGene5
+            // 
+            this.ButtonGene5.Location = new System.Drawing.Point(150, 253);
+            this.ButtonGene5.Name = "ButtonGene5";
+            this.ButtonGene5.Size = new System.Drawing.Size(75, 23);
+            this.ButtonGene5.TabIndex = 20;
+            this.ButtonGene5.Text = "Generuj5!";
+            this.ButtonGene5.UseVisualStyleBackColor = true;
+            this.ButtonGene5.Click += new System.EventHandler(this.ButtonGene5_Click);
+            // 
+            // ButtonGene10
+            // 
+            this.ButtonGene10.Location = new System.Drawing.Point(150, 399);
+            this.ButtonGene10.Name = "ButtonGene10";
+            this.ButtonGene10.Size = new System.Drawing.Size(75, 23);
+            this.ButtonGene10.TabIndex = 25;
+            this.ButtonGene10.Text = "Generuj10!";
+            this.ButtonGene10.UseVisualStyleBackColor = true;
+            this.ButtonGene10.Click += new System.EventHandler(this.ButtonGene10_Click);
+            // 
+            // ButtonGene9
+            // 
+            this.ButtonGene9.Location = new System.Drawing.Point(150, 370);
+            this.ButtonGene9.Name = "ButtonGene9";
+            this.ButtonGene9.Size = new System.Drawing.Size(75, 23);
+            this.ButtonGene9.TabIndex = 24;
+            this.ButtonGene9.Text = "Generuj9!";
+            this.ButtonGene9.UseVisualStyleBackColor = true;
+            this.ButtonGene9.Click += new System.EventHandler(this.ButtonGene9_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(107, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(219, 20);
+            this.textBox1.TabIndex = 26;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(107, 106);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(219, 20);
+            this.textBox2.TabIndex = 27;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(154, 428);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 13);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "Patrick P-C# And General Code in C#";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 428);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 13);
+            this.label2.TabIndex = 29;
+            this.label2.Text = "Bartek T-  HTML(Big Brain)";
+            // 
+            // RIG
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ButtonGene10);
+            this.Controls.Add(this.ButtonGene9);
+            this.Controls.Add(this.ButtonGene8);
+            this.Controls.Add(this.ButtonGene7);
+            this.Controls.Add(this.ButtonGene6);
+            this.Controls.Add(this.ButtonGene5);
+            this.Controls.Add(this.ButtonGene4);
+            this.Controls.Add(this.ButtonGene3);
+            this.Controls.Add(this.ButtonGene2);
+            this.Controls.Add(this.ButtonGene1);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txFiltr);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txTitle);
+            this.Controls.Add(this.txOpis);
+            this.Controls.Add(this.txIdea);
+            this.Name = "RIG";
+            this.Text = "Random Idea Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txIdea;
+        private System.Windows.Forms.Label txOpis;
+        private System.Windows.Forms.Label txTitle;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label txFiltr;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button ButtonGene1;
+        private System.Windows.Forms.Button ButtonGene2;
+        private System.Windows.Forms.Button ButtonGene4;
+        private System.Windows.Forms.Button ButtonGene3;
+        private System.Windows.Forms.Button ButtonGene8;
+        private System.Windows.Forms.Button ButtonGene7;
+        private System.Windows.Forms.Button ButtonGene6;
+        private System.Windows.Forms.Button ButtonGene5;
+        private System.Windows.Forms.Button ButtonGene10;
+        private System.Windows.Forms.Button ButtonGene9;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class RIG : Form
+    {
+        public RIG()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonGene_Click(object sender, EventArgs e)
+        {
+            string[] tab = new string[4] { "Helicopter", "Machine Of Destrucion", "Armor", "Gun" };
+            Random random = new Random();
+            int index = random.Next(0, 3);
+            string text = tab[index];
+            txIdea.Text = text;
+
+            string[] tab2 = new string[3] { "with", "on", "linked to" };
+            
+            int index2 = random.Next(0, 3);
+            string text2 = tab2[index2];
+            textBox1.Text = text2;
+
+            string[] tab3 = new string[3] {"Mech" , "OP Car" , "Human" };
+            
+            int index3 = random.Next(0, 3);
+            string text3 = tab3[index3];
+            textBox2.Text = text3;
+
+        }
+
+        private void ButtonGene2_Click(object sender, EventArgs e)
+        {
+            string[] tab = new string[3] { "Wooden", "Wooden & Stone", "Stone" };
+            Random random = new Random();
+            int index = random.Next(0, 3);
+            string text = tab[index];
+            txIdea.Text = text;
+
+            string[] tab2 = new string[3] { "House", "Safe House", "Staute" };
+
+            int index2 = random.Next(0, 3);
+            string text2 = tab2[index2];
+            textBox1.Text = text2;
+
+            string[] tab3 = new string[3] { "With redstone", "On survival", "On creative" };
+
+            int index3 = random.Next(0, 3);
+            string text3 = tab3[index3];
+            textBox2.Text = text3;
+
+        }
+
+        private void ButtonGene3_Click(object sender, EventArgs e)
+        {
+            string[] tab = new string[3] { "Trap on", "Colleting", "Playing(with)" };
+            Random random = new Random();
+            int index = random.Next(0, 3);
+            string text = tab[index];
+            txIdea.Text = text;
+
+            string[] tab2 = new string[3] { "Rabbits(leather)", "Friends", "Caniballs(bones)" };
+
+            int index2 = random.Next(0, 3);
+            string text2 = tab2[index2];
+            textBox1.Text = text2;
+
+            string[] tab3 = new string[3] { "in day", "in night", "in developer mode" };
+
+            int index3 = random.Next(0, 3);
+            string text3 = tab3[index3];
+            textBox2.Text = text3;
+        }
+
+        private void ButtonGene4_Click(object sender, EventArgs e)
+        {
+            string[] tab = new string[7] { "V-tol", "Gun", "Human", "Cyborg", "Metal", "Wooden", "Concrete" };
+            Random random = new Random();
+            int index = random.Next(0, 7);
+            string text = tab[index];
+            txIdea.Text = text;
+
+            string[] tab2 = new string[3] { "With in", "From", "Out Of (a)" };
+
+            int index2 = random.Next(0, 3);
+            string text2 = tab2[index2];
+            textBox1.Text = text2;
+
+            string[] tab3 = new string[12] { "Alien", "Shit", "Sheet", "Syringe", "Robot", "Flail", "Mace", "Siege", "Canon", "Masterpiece", "Deep", "Deep within" };
+
+            int index3 = random.Next(0, 12);
+            string text3 = tab3[index3];
+            textBox2.Text = text3;
+        }
+
+        private void ButtonGene5_Click(object sender, EventArgs e)
+        {
+            string[] tab = new string[15] { "V-tol", "Gun", "Human", "Cyborg", "Metal", "Wooden", "Concrete", "Plane", "Car", "Boat", "Submarine", "Hovercraft", "Bike", "Volt-bike", "Puppet" };
+            Random random = new Random();
+            int index = random.Next(0, 15);
+            string text = tab[index];
+            txIdea.Text = text;
+
+            string[] tab2 = new string[3] { "With in", "From", "Out Of (a)" };
+
+            int index2 = random.Next(0, 3);
+            string text2 = tab2[index2];
+            textBox1.Text = text2;
+
+            string[] tab3 = new string[12] { "Alien", "Shit", "Sheet", "Syringe", "Robot", "Flail", "Mace", "Siege", "Canon", "Masterpiece", "Deep", "Deep within" };
+
+            int index3 = random.Next(0, 12);
+            string text3 = tab3[index3];
+            textBox2.Text = text3;
+        }
+
+        private void ButtonGene6_Click(object sender, EventArgs e)
+        {
+            string[] tab = new string[6] { "Alien", "Bonzai Tree", "Car", "Human", "Design", "Anime" };
+            Random random = new Random();
+            int index = random.Next(0, 3);
+            string text = tab[index];
+            txIdea.Text = text;
+
+            string[] tab2 = new string[3] { "With(in)", "On", "In"};
+
+            int index2 = random.Next(0, 3);
+            string text2 = tab2[index2];
+            textBox1.Text = text2;
+
+            string[] tab3 = new string[7] { "Moutain", "Cave", "TV", "Game", "Clothes", "Gun", "City" };
+
+            int index3 = random.Next(0, 6);
+            string text3 = tab3[index3];
+            textBox2.Text = text3;
+        }
+
+        private void ButtonGene7_Click(object sender, EventArgs e)
+        {
+            string[] tab = new string[3] { "Build", "Get", "Destroy" };
+            Random random = new Random();
+            int index = random.Next(0, 3);
+            string text = tab[index];
+            txIdea.Text = text;
+
+            string[] tab2 = new string[3] { "Hellevator(Items)", "NPCS", "Big house/Structure in the desert" };
+
+            int index2 = random.Next(0, 3);
+            string text2 = tab2[index2];
+            textBox1.Text = text2;
+
+            string[] tab3 = new string[3] { "Without Normal or Expert only Master Mode", "With Only Buyable Block/Items From NPC", "With only Natural Blocks/Cave Items" };
+
+            int index3 = random.Next(0, 3);
+            string text3 = tab3[index3];
+            textBox2.Text = text3;
+        }
+
+        private void ButtonGene8_Click(object sender, EventArgs e)
+        {
+            string[] tab = new string[8] { "Farm", "Stone", "Tek", "Platform", "Mastercraft", "Yourneyman", "Ammo", "Get Every Dinosaur" };
+            Random random = new Random();
+            int index = random.Next(0, 8);
+            string text = tab[index];
+            txIdea.Text = text;
+
+            string[] tab2 = new string[5] { "Refill", "Gun", "That is", "For", "Of" };
+
+            int index2 = random.Next(0, 5);
+            string text2 = tab2[index2];
+            textBox1.Text = text2;
+
+            string[] tab3 = new string[3] { "Station", "Gym", "Practical" };
+
+            int index3 = random.Next(0, 3);
+            string text3 = tab3[index3];
+            textBox2.Text = text3;
+        }
+
+        private void ButtonGene9_Click(object sender, EventArgs e)
+        {
+            string[] tab = new string[8] { "Farm", "Stone", "Tek", "Platform", "Mastercraft", "Yourneyman", "Ammo", "Get Every Dinosaur" };
+            Random random = new Random();
+            int index = random.Next(0, 8);
+            string text = tab[index];
+            txIdea.Text = text;
+
+            string[] tab2 = new string[5] { "Refill", "Gun", "That is", "For", "Of" };
+
+            int index2 = random.Next(0, 5);
+            string text2 = tab2[index2];
+            textBox1.Text = text2;
+
+            string[] tab3 = new string[3] { "Station", "Gym", "Practical" };
+
+            int index3 = random.Next(0, 3);
+            string text3 = tab3[index3];
+            textBox2.Text = text3;
+        }
+
+        private void ButtonGene10_Click(object sender, EventArgs e)
+        {
+            string[] tab = new string[2] { "Make", "Create" };
+            Random random = new Random();
+            int index = random.Next(0, 2);
+            string text = tab[index];
+            txIdea.Text = text;
+
+            string[] tab2 = new string[2] { "A", "A an" };
+
+            int index2 = random.Next(0, 2);
+            string text2 = tab2[index2];
+            textBox1.Text = text2;
+
+            string[] tab3 = new string[7] { "Landscape", "Car", "Plane", "Gun", "Melee Weapon", "Boat", "Animation With SMTH" };
+
+            int index3 = random.Next(0, 7);
+            string text3 = tab3[index3];
+            textBox2.Text = text3;
+        }
+    }
+}
